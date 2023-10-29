27127fed96717f4cc25a90fe1efb47d64a2d30e3
vi <letrieuvi1209@gmail.com>
2023-10-29 23:57:22 +0700

    Tao trang cap nhat lien he

--- src/views/ContactBook.vue
+++ src/views/ContactBook.vue
@@ -29,13 +29,23 @@
                     <i class="fas fa-address-card"></i>
                 </h4>
                 <ContactCard :contact="activeContact" />
+                <router-link
+					:to="{
+						name: 'contact.edit',
+						params: { id: activeContact._id },
+					}"
+				>
+					<span class="mt-2 badge badge-warning">
+						<i class="fas fa-edit"></i> Hiệu chỉnh
+					</span>
+				</router-link>
             </div>
 
-            <button type="button" class="btn btn-warning" style="font-size: 10px; padding: 1px; font-weight: bold;"
+            <!-- <button type="button" class="btn btn-warning" style="font-size: 10px; padding: 1px; font-weight: bold;"
             v-if="activeIndex!==-1">
             <i class="fa-solid fa-pen-to-square"></i>
                 Hiệu Chỉnh
-            </button>
+            </button> -->
         </div>
     </div>
 </template>
